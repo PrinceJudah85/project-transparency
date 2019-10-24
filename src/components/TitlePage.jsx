967c6371ab38d27f78e535fab5b44d1d817623e4
Rafael Castano <rafa328@gmail.com>
2019-10-24 12:17:48 -0400

    meta link tags updated

--- src/components/TitlePage.jsx
+++ src/components/TitlePage.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import HoCTheme from '../HoCTheme.mp3'
 
-export default function TitlePage() {
-  return (
-    <Link to="/main">
-      <div id="title-page">
-        <h1 className="title-font"> Transparency </h1>
-        <p id="quote">"Those who would give up essential Liberty, to purchase a little temporary Safety, deserve neither Liberty nor Safety."<br /> <br /> - Benjamin Franklin</p>
-      </div>
-    </Link>
-  )
+class TitlePage extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    let audio = new Audio(HoCTheme);
+    return (
+      <Link to="/main" onClick={async () => await
+        audio.play()} >
+        <div id="title-page">
+          <h1 className="title-font"> Transparency </h1>
+          <p id="quote">"Those who would give up essential Liberty, to purchase a little temporary Safety, deserve neither Liberty nor Safety."<br /> <br /> - Benjamin Franklin</p>
+        </div>
+      </Link >
+    )
+  }
 }
+export default TitlePage;
